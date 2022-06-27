071f9a1deeaa776a9b56e94de58327a89cd3a7af
Alex <a.tsiklauri@gmail.com>
2022-06-27 09:17:17 +0100

    init: push complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let abs = Math.abs(n);
+  let str = abs.toString();
+  let num3 = parseInt(str.charAt(0));
+  let num2 = parseInt(str.charAt(1));
+  let num1 = parseInt(str.charAt(2));
+  return num1 * 100 + num2 * 10 + num3;
 }
